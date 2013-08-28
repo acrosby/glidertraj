aca9e019cf1e3d2a3635eb5c8cbce8abb115fa24
Alexander Crosby <acrosby@asascience.com>
2013-08-28 11:23:02 -0400

    newgeojson, based on 100 stride of test dataset

--- ioos_glider.py
+++ ioos_glider.py
@@ -1088,7 +1088,7 @@ if __name__ == "__main__":
         with Dataset(dap) as nc:
             lon = nc.variables["lon"][:].flatten().data.astype(np.float64)
             lat = nc.variables["lat"][:].flatten().data.astype(np.float64)
-            coords = zip(lon[lon<1000][:100], lat[lat<1000][:100])
+            coords = zip(lon[lon<1000][::100], lat[lat<1000][::100])
             #coords = zip(np.ones((100,)), np.ones((100,)))
             n = gj.LineString( coords )
             s = getncattrs(nc)
